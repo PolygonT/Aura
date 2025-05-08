f3de461bc43f2b5c80564106c5c91cc52ead3e0c
wenhaoxiong <formalityefqaq@gmail.com>
2025-05-08 19:05:48 +0800

    update

--- Source/Aura/Private/Charactor/BaseCharactor.cpp
+++ Source/Aura/Private/Charactor/BaseCharactor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BaseCharactor.h"
+#include "Character/BaseCharactor.h"
 
 // Sets default values
 ABaseCharactor::ABaseCharactor()
